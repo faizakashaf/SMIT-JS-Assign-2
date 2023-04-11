af6035cbf5e3db7be2446ce2ff9b86c6a38a88f3
anasnaeem80 <109957830+anasnaeem80@users.noreply.github.com>
2023-04-12 04:33:16 +0500

    Ques 2
    Write a recursive function that searches an array for a specific value. The
    function should return true if the value is found, and false if it is not. You
    can assume that the array is not nested.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-function createAdder(addition) {
-    return function (num) {
-      return num + addition;
-    };
+function searchArray(arr, value) {
+    if (arr.length === 0) {
+      return false;
+    }
+  
+      if (arr[0] === value) {
+      return true;
+    }
+  
+    return searchArray(arr.slice(1), value);
   }
   
-  var add5 = createAdder(5);
-  var result = add5(10); 
+  // Example usage:
+  const myArray = [1, 2, 3, 4, 5];
+  const searchValue = 3;
+  console.log(searchArray(myArray, searchValue)); // Output: true
